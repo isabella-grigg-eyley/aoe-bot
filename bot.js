4c62ce55cd482da29e23a388bea2aad55a015f06
isabella-grigg-eyley <isabella.grigg.eyley@gmail.com>
2018-10-06 20:47:05 +1300

    basically working

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const { Client, Attachment } = require('discord.js')
 const client = new Client()
 const { token } = require('./auth.json')
-const aoe = require('./aoe-commands.json')
+const aoeCommands = require('./aoe-commands.json')
+const aoeMp3s = require('./aoe-mp3s.json')
 
 
 client.on('ready', () => {
@@ -11,17 +12,28 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.content.substring(0, 1) === '!') {
     if (message.content.substring(0, 4) == '!aoe') {
-      let args = message.content.split(' ')
-      let cmd = Number(args[1])
-      if (cmd <= 42 && cmd > 0) {
-        let phrase = aoe[cmd]
-        message.channel.send(phrase, { tts: true })
-      }
+      aoePhrases(message)
     } else if (message.content.substring(0, 7) == "!wololo") {
-      const attachment = new Attachment('./wololo.png')
-      message.channel.send(attachment)
+      wololo(message)
     }
   }
 })
+
+function aoePhrases(message) {
+  let args = message.content.split(' ')
+  let cmd = Number(args[1])
+  if (cmd <= 42 && cmd > 0) {
+    //let phrase = aoeCommands[cmd]
+    //message.channel.send(phrase, { tts: true })
+    const attachment = new Attachment(aoeMp3s[cmd])
+    console.log(aoeMp3s[cmd])
+    message.channel.send(attachment)
+  }
+}
+
+function wololo(message) {
+  const attachment = new Attachment('./wololo.png')
+  message.channel.send(attachment)
+}
 
 client.login(token);
