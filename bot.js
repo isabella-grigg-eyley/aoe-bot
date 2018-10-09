68a427434e3de4642555ebcccfca7b6dae2b937f
isabella-grigg-eyley <isabella.grigg.eyley@gmail.com>
2018-10-10 12:52:01 +1300

    moar

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const aoeCommands = require('./aoe-commands.json')
 const aoeMp3sData = require('./aoe-mp3s.json')
 const startupCommands = require('./startups')
 const playstationSounds = require('./playstation.json')
+const miscSounds = require('./misc.json')
 
 let ready = true;
 
@@ -26,12 +27,21 @@ client.on('message', message => {
       } else if (message.content.substring(0, 7) == "!wololo" && ready == true) {
         ready = false;
         wololo(message)
-      } else if (message.content.substring(0, 3) == "!ps1" && ready == true) {
+      } else if (message.content.substring(0, 4) == "!ps1" && ready == true) {
         ready = false;
         playstation(message, '1')
-      } else if (message.content.substring(0, 3) == "!ps2" && ready == true) {
+      } else if (message.content.substring(0, 4) == "!ps2" && ready == true) {
         ready = false;
         playstation(message, '2')
+      } else if (message.content.substring(0, 4) == "!law" && ready == true) {
+        ready = false;
+        playMisc(message, 'law')
+      } else if (message.content.substring(0, 13) == "!is-only-game" && ready == true) {
+        ready = false;
+        playMisc(message, 'is-only-game')
+      } else if (message.content.substring(0, 13) == "!x-files" && ready == true) {
+        ready = false;
+        playMisc(message, 'x-files')
       }
     }
   } catch (err) {
@@ -39,6 +49,27 @@ client.on('message', message => {
   }
 })
 
+function playMisc(message, filename) {
+  let voiceChannel = message.member.voiceChannel
+  if (message.member.voiceChannel == null) {
+    message.channel.send('Get in vc ya scrub')
+  } else {
+    voiceChannel
+      .join()
+      .then(connection => {
+        const dispatcher = connection.playFile(miscSounds[filename])
+        dispatcher.on('end', end => {
+          voiceChannel.leave()
+        })
+      })
+      .catch(err => {
+        message.channel.send('AOE bot has resigned')
+        console.log(err)
+        voiceChannel.leave()
+      })
+  }
+  ready = true
+}
 
 function playstation(message, num) {
   let voiceChannel = message.member.voiceChannel
