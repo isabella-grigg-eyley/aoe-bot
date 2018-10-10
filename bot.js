ec512d172c0dfe37beb0b9195ba479616224a286
isabella-grigg-eyley <isabella.grigg.eyley@gmail.com>
2018-10-10 13:02:16 +1300

    blegh

--- bot.js
+++ bot.js
@@ -23,7 +23,6 @@ client.on('message', message => {
       if (message.content.substring(0, 4) == '!aoe' && ready == true) {
         ready = false;
         aoeMp3s(message)
-
       } else if (message.content.substring(0, 7) == "!wololo" && ready == true) {
         ready = false;
         wololo(message)
@@ -39,9 +38,21 @@ client.on('message', message => {
       } else if (message.content.substring(0, 13) == "!is-only-game" && ready == true) {
         ready = false;
         playMisc(message, 'is-only-game')
-      } else if (message.content.substring(0, 13) == "!x-files" && ready == true) {
+      } else if (message.content.substring(0, 8) == "!x-files" && ready == true) {
         ready = false;
         playMisc(message, 'x-files')
+      } else if (message.content.substring(0, 9) == "!xp-start" && ready == true) {
+        ready = false;
+        playMisc(message, 'xp-start')
+      } else if (message.content.substring(0, 7) == "!xp-end" && ready == true) {
+        ready = false;
+        playMisc(message, 'xp-end')
+      } else if (message.content.substring(0, 8) == "!pinball" && ready == true) {
+        ready = false;
+        playMisc(message, 'pinball')
+      } else if (message.content.substring(0, 9) == "!help" && ready == true) {
+        let aoebotChannel = client.channels.get('392199619758784523')
+        aoebotChannel.send("The commands available are !aoe [number between 1 and 42], !wololo, !ps1, !ps2, !law, !is-only-game, !x-files, !xp-start, !xp-end and !pinball.")
       }
     }
   } catch (err) {
