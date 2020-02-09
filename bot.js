efc3a7fca49897510aec7d6bc02e4e11aa49a9e4
isabella-grigg-eyley <isabella.grigg.eyley@gmail.com>
2020-02-09 19:16:02 +1300

    Formatting!

--- bot.js
+++ bot.js
@@ -1,111 +1,111 @@
 // discord config and modules
 const
-{
-  Client
-} = require('discord.js')
-const client = new Client()
+	{
+		Client
+	} = require('discord.js');
+const client = new Client();
 
 // auth
 
-require('dotenv').config()
-const token = process.env.TOKEN
+require('dotenv').config();
+const token = process.env.TOKEN;
 
 // local vars
 
-let ready = true
+let ready = true;
 
 // functions
 
 const
-{
-  sendStartupMessage,
-  aoeMp3s,
-  wololo,
-  playstation,
-  playMisc,
-  playFma,
-  sendHelp
-} = require('./commands')
+	{
+		SendStartupMessage,
+		AOEMP3s,
+		Wololo,
+		Playstation,
+		PlayMisc,
+		PlayFma,
+		SendHelp
+	} = require('./commands');
 
 
-client.on('ready', () => 
+client.on('ready', () =>
 {
-  console.log('ready!')
-  //sendStartupMessage(client)
-})
+	console.log('ready!');
+	//SendStartupMessage(client)
+});
 
-client.on('message', message => 
+client.on('message', message =>
 {
-  try
-  {
-    if (!ready) console.log("Message received but still waiting to finish previous command")
-    
-    if (message.content.substring(0, 1) === '!' && ready)
-    {
-      let cmd = grabInitCommand(message.content)
-      
-      switch (cmd)
-      {
-        case 'help':
-          ready = false
-          sendHelp(message, setReadyTrue)
-          break;
-        case 'aoe':
-          ready = false
-          aoeMp3s(message, setReadyTrue)
-          break;
-        case 'wololo':
-          ready = false
-          wololo(message, setReadyTrue)
-          break;
-        case 'fma':
-          ready = false
-          playFma(message, setReadyTrue)
-          break;
-        case 'ps1':
-        case 'ps2':
-          ready = false
-          playstation(message, setReadyTrue)
-          break;
-        case 'law':
-        case 'is-only-game':
-        case 'x-files':
-        case 'xp-start':
-        case 'xp-end':
-        case 'pinball':
-        case 'adum':
-        case 'deathnote':
-        case 'halloween':
-          ready = false
-          playMisc(message, setReadyTrue)
-          break;
-        default:
-          console.log(`invalid command detected: ${cmd}`)
-          setReadyTrue()
-          break;
-      }
-    }
-  }
-  catch (err)
-  {
-    console.log(`ERROR: ${err}`)
-    setReadyTrue()
-  }
-})
+	try
+	{
+		if (!ready) console.log("Message received but still waiting to finish previous command");
+		
+		if (message.content.substring(0, 1) === '!' && ready)
+		{
+			let cmd = GrabInitCommand(message.content);
+			
+			switch (cmd)
+			{
+				case 'help':
+					ready = false;
+					SendHelp(message, SetReadyTrue);
+					break;
+				case 'aoe':
+					ready = false;
+					AOEMP3s(message, SetReadyTrue);
+					break;
+				case 'wololo':
+					ready = false;
+					Wololo(message, SetReadyTrue);
+					break;
+				case 'fma':
+					ready = false;
+					PlayFma(message, SetReadyTrue);
+					break;
+				case 'ps1':
+				case 'ps2':
+					ready = false;
+					Playstation(message, SetReadyTrue);
+					break;
+				case 'law':
+				case 'is-only-game':
+				case 'x-files':
+				case 'xp-start':
+				case 'xp-end':
+				case 'pinball':
+				case 'adum':
+				case 'deathnote':
+				case 'halloween':
+					ready = false;
+					PlayMisc(message, SetReadyTrue);
+					break;
+				default:
+					console.log(`invalid command detected: ${cmd}`);
+					SetReadyTrue();
+					break;
+			}
+		}
+	}
+	catch (err)
+	{
+		console.log(`ERROR: ${err}`);
+		SetReadyTrue();
+	}
+});
 
-function setReadyTrue()
+function SetReadyTrue()
 {
-  ready = true
+	ready = true;
 }
 
-function grabInitCommand(cmd)
+function GrabInitCommand(cmd)
 {
-  let spaceIdx = cmd.indexOf(' ')
-  if (spaceIdx > -1)
-    cmd = cmd.substring(1, spaceIdx)
-  else
-    cmd = cmd.substring(1)
-  return cmd
+	let spaceIdx = cmd.indexOf(' ');
+	if (spaceIdx > -1)
+		cmd = cmd.substring(1, spaceIdx);
+	else
+		cmd = cmd.substring(1);
+	return cmd;
 }
 
 client.login(token);
--- commands.js
+++ commands.js
@@ -1,106 +1,109 @@
 // discord config and modules
 const
-{
-  Attachment
-} = require('discord.js')
+	{
+		Attachment
+	} = require('discord.js');
 
 // data refs
 
-const startupCommands = require('./misc-fx/startups')
-const aoeMp3sData = require('./sfx/aoe-mp3s.json')
-const playstationSounds = require('./sfx/playstation.json')
-const miscSounds = require('./sfx/misc.json')
+const startupCommands = require('./misc-fx/startups');
+const aoeMp3sData = require('./sfx/aoe-mp3s.json');
+const playstationSounds = require('./sfx/playstation.json');
+const miscSounds = require('./sfx/misc.json');
 
 //local vars
 
-let fmaBool = true
+let fmaBool = true;
 
 
-function sendStartupMessage(client)
+function SendStartupMessage(client)
 {
-  let aoebotChannel = client.channels.get('392199619758784523')
-  let rand = Math.floor(Math.random() * 3)
-  aoebotChannel.send(startupCommands[rand])
+	let aoebotChannel = client.channels.get('392199619758784523');
+	let rand = Math.floor(Math.random() * 3);
+	aoebotChannel.send(startupCommands[rand]);
 }
 
-function playFile(message, filename, cb)
+function PlayFile(message, filename, cb)
 {
-  let voiceChannel = message.member.voiceChannel
-
-  if (message.member.voiceChannel == null)
-    message.channel.send('Get in vc ya scrub')
-
-  else
-  {
-    voiceChannel
-      .join()
-      .then(connection => {
-        const dispatcher = connection.playFile(filename)
-        dispatcher.on('end', end => {
-          voiceChannel.leave()
-          cb()
-        })
-      })
-      .catch(err => {
-        message.channel.send('AOE bot has resigned')
-        console.log(err)
-        voiceChannel.leave()
-        cb()
-      })
-  }
-
+	let voiceChannel = message.member.voiceChannel;
+	
+	if (message.member.voiceChannel == null)
+		message.channel.send('Get in vc ya scrub');
+	
+	else
+	{
+		voiceChannel
+			.join()
+			.then(connection =>
+			{
+				const dispatcher = connection.playFile(filename);
+				dispatcher.on('end', end =>
+				{
+					voiceChannel.leave();
+					cb();
+				});
+			})
+			.catch(err =>
+			{
+				message.channel.send('AOE bot has resigned');
+				console.log(err);
+				voiceChannel.leave();
+				cb();
+			});
+	}
+	
 }
 
-function playMisc(message, cb)
+function PlayMisc(message, cb)
 {
-  let filename = message.content.split(' ')[0].substring(1)
-  playFile(message, miscSounds[filename], cb)
+	let filename = message.content.split(' ')[0].substring(1);
+	PlayFile(message, miscSounds[filename], cb);
 }
 
-function playstation(message, cb)
+function Playstation(message, cb)
 {
-  let num = message.content.substring(3, 4)
-  playFile(message, playstationSounds[num], cb)
+	let num = message.content.substring(3, 4);
+	PlayFile(message, playstationSounds[num], cb);
 }
 
-function aoeMp3s(message, cb)
+function AOEMP3s(message, cb)
 {
-  let args = message.content.split(' ')
-  let cmd = Number(args[1])
-  if (cmd <= 42 && cmd > 0)
-    playFile(message, aoeMp3sData[cmd], cb)
-  else
-    cb()
+	let args = message.content.split(' ');
+	let cmd = Number(args[1]);
+	if (cmd <= 42 && cmd > 0)
+		PlayFile(message, aoeMp3sData[cmd], cb);
+	else
+		cb();
 }
 
-function wololo(message, cb)
+function Wololo(message, cb)
 {
-  const attachment = new Attachment('./misc-fx/wololo.png')
-  message.channel.send(attachment)
-  playFile(message, aoeMp3sData[30], cb)
+	const attachment = new Attachment('./misc-fx/wololo.png');
+	message.channel.send(attachment);
+	PlayFile(message, aoeMp3sData[30], cb);
 }
 
-function playFma(message, cb)
+function PlayFma(message, cb)
 {
-  let str;
-  fmaBool ? str = "fma" + 1 : str = "fma" + 2
-  fmaBool = !fmaBool
-  playFile(message, miscSounds[str], cb)
+	let str;
+	fmaBool ? str = "fma" + 1 : str = "fma" + 2;
+	fmaBool = !fmaBool;
+	PlayFile(message, miscSounds[str], cb);
 }
 
-function sendHelp(message, cb)
+function SendHelp(message, cb)
 {
-  message.channel.send('**Hello!**\nThe commands available are:\n\n!aoe *[number between 1 and 42]*\n!wololo\n!fma\n!ps1\n!ps2\n!law\n!is-only-game\n!x-files\n!xp-start\n!xp-end\n!pinball\n!adum\n!deathnote\n!halloween\n!help')
-  cb()
+	message.channel.send('**Hello!**\nThe commands available are:\n\n!aoe *[number between 1 and 42]*\n!wololo\n!fma\n!ps1\n!ps2\n!law\n!is-only-game\n!x-files\n!xp-start\n!xp-end\n!pinball\n!adum\n!deathnote\n!halloween\n!help');
+	cb();
 }
 
 module.exports =
-{
-  sendStartupMessage,
-  aoeMp3s,
-  wololo,
-  playstation,
-  playMisc,
-  playFma,
-  sendHelp
-}
+	{
+		SendStartupMessage: SendStartupMessage,
+		AOEMP3s: AOEMP3s,
+		Wololo: Wololo,
+		Playstation: Playstation,
+		PlayMisc: PlayMisc,
+		PlayFma: PlayFma,
+		SendHelp: SendHelp
+	};
